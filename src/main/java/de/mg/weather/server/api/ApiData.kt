f40816590362a2709051b639a7020a5dbb397a71
Michael <spa1@trali.de>
2018-06-15 23:42:38 +0200

    implemented rest controller

--- src/main/java/de/mg/weather/server/api/ApiData.kt
+++ src/main/java/de/mg/weather/server/api/ApiData.kt
@@ -2,6 +2,12 @@ package de.mg.weather.server.api
 
 import de.mg.weather.server.model.SensorEnum
 
-class ApiData(val series: List<SensorEnum>, val data: List<List<Long>>, current: Map<SensorEnum, SensorTimeValue>)
+class ApiData(
+        val series: List<SensorEnum>,
 
-class SensorTimeValue(time: Long, value: Long)
+        /** first list = x axis, second list = x value and y values for all series **/
+        val data: List<List<Long>>,
+
+        current: Map<SensorEnum, SensorTimeValue>)
+
+class SensorTimeValue(time: Long, value: Float)
--- src/main/java/de/mg/weather/server/api/RestController.kt
+++ src/main/java/de/mg/weather/server/api/RestController.kt
@@ -2,7 +2,11 @@ package de.mg.weather.server.api
 
 
 import de.mg.weather.server.model.SensorData
+import de.mg.weather.server.model.SensorEnum
+import de.mg.weather.server.service.Utils.epoch
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.RequestMapping
+import java.time.LocalDateTime
 
 @org.springframework.web.bind.annotation.RestController
 class RestController {
@@ -10,12 +14,47 @@ class RestController {
     @Autowired
     private lateinit var sensorData: SensorData
 
-/*
+
+    @Suppress("FoldInitializerAndIfToElvis")
     @RequestMapping(value = "/data", produces = ["application/json"])
     fun monthlySums(): ApiData {
 
-        return ApiData()
+        val currentValuesMap = SensorEnum.values().map { type ->
+            val last = sensorData.sensorsMap[type]!!.lastReceived.get()
+            type to SensorTimeValue(epoch(last.time), last.value)
+        }.toMap()
+
+
+        val dataMap = mutableMapOf<LocalDateTime, MutableMap<SensorEnum, Float>>()
+        SensorEnum.values().forEach { type ->
+
+            sensorData.sensorsMap[type]!!.values.forEach { entry ->
+                var sensorValues = dataMap[entry.time]
+                if (sensorValues == null) {
+                    sensorValues = mutableMapOf()
+                    dataMap[entry.time] = sensorValues
+                }
+                sensorValues[type] = entry.value
+            }
+        }
+
+
+        val dataList = dataMap.keys.sorted().map { time ->
+
+            val entryList = mutableListOf(epoch(time))
+            SensorEnum.values().forEach { type ->
+                val sensorValues = dataMap[time]
+                if (sensorValues != null) {
+                    val sensorTypeValue = sensorValues[type]?.toLong()
+                    if (sensorTypeValue != null)
+                        entryList.add(sensorTypeValue)
+                }
+            }
+            entryList.toList()
+        }
+
+        return ApiData(SensorEnum.values().toList(), dataList, currentValuesMap)
     }
-*/
+
 
 }
